5c84cb52615ec2062309cf240efdbd099cbcef15
LF <LF@llbt.local>
2016-06-13 14:21:17 +0800

    Initial Commit

---
+++ EaseShop/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EaseShop
+//
+//  Created by LF on 16/6/13.
+//  Copyright © 2016年 LF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
